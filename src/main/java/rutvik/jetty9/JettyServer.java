67c5d4c85767cbcb3b64c2aea3b2795818d5463b
Rutvik Pawale <rutwik268@gmail.com>
2024-01-29 15:48:22 +0530

    Add files via upload

---
+++ src/main/java/rutvik/jetty9/JettyServer.java
@@ -0,0 +1,17 @@
+package rutvik.jetty9;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+
+public class JettyServer {
+	private Server server;
+	
+	public void start() throws Exception {
+		server = new Server();
+		ServerConnector connector = new ServerConnector(server);
+		connector.setPort(8090);
+		server.setConnectors(new Connector[] {connector});
+		
+	}
+}
